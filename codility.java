1e6dbec0de6b1700bc6f8a2e0c41e965f00eb9f1
pvanhungit <pvanhungit@gmail.com>
2024-03-18 19:38:31 +0700

    Update codility.java

--- codility.java
+++ codility.java
@@ -1,1 +1,34 @@
+//CreateLongestSpike
 
+// you can also use imports, for example:
+ import java.util.*;
+
+// you can write to stdout for debugging purposes, e.g.
+// System.out.println("this is a debug message");
+
+class Solution {
+    public int solution(int[] A) {
+        // Implement your solution here
+        Arrays.sort(A);
+        int len = A.length, cnt = 1, max = A[len - 1];
+        int lastValue = A[0], currentCnt = 1;
+        for(int i = 1 ; i < len; i++) {
+            if(A[i] == max) {
+                cnt++;
+                break;
+            }
+            if(A[i] == A[i-1]) {
+                if(currentCnt < 2) {
+                    currentCnt++;
+                    cnt++;
+                }
+            } else {
+                lastValue = A[i];
+                currentCnt = 1;
+                cnt++;
+            }
+
+        }
+        return cnt;
+    }
+}
